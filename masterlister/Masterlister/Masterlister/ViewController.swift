594a583242433a4a84deaafdcbf671583a75f80b
Alex Curylo <alex@alexcurylo.com>
2017-07-20 10:10:15 +0700

    Create Masterlister project - Xcode 9b3

---
+++ masterlister/Masterlister/Masterlister/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Masterlister
+//
+//  Created by Curylo, Alex (Agoda) on 7/20/17.
+//  Copyright © 2017 Trollwerks Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
