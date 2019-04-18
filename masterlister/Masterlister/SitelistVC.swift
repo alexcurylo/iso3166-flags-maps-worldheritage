0df00d709ab1473e46cacae06ad95892a73bcccb
Alex Curylo <alex@alexcurylo.com>
2019-04-19 11:13:31 +1200

    Add Sichuan visits

--- masterlister/Masterlister/SitelistVC.swift
+++ masterlister/Masterlister/SitelistVC.swift
@@ -273,9 +273,9 @@ final class SitelistVC: NSViewController {
     }
 
     private func writeFooter(for type: Document) {
-        assert(whsVisited.count == 492, "Should be 492 WHS visited not \(whsVisited.count) (2019.03.12)")
-        assert(twhsVisited.count == 342, "Should be 342 TWHS visited not \(twhsVisited.count) (2019.04.03)")
         mtpChecker.checkMissing()
+        assert(whsVisited.count == 494, "Should be 494 WHS visited not \(whsVisited.count) (2019.04.19)")
+        assert(twhsVisited.count == 344, "Should be 344 TWHS visited not \(twhsVisited.count) (2019.04.19)")
         // swiftlint:disable:next line_length
         let updatesURL = "http://whc.unesco.org/en/tentativelists/?action=listtentative&pattern=&state=&theme=&criteria_restrication=&date_start=11%2F03%2F2018&date_end=&order=year"
 
